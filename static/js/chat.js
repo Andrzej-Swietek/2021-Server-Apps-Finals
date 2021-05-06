e021003b3a0e694a530ef79729b936021ce27251
AndrzejSwietek2001 <mr.andi321@gmail.com>
2021-05-07 01:42:35 +0200

    chat style

--- static/js/chat.js
+++ static/js/chat.js
@@ -12,16 +12,16 @@ window.addEventListener('DOMContentLoaded', ()=>{
     socket.on('chatMessage', (message) => {
         console.log(message);
         // Scroll down
-
+        let author = 'Andrzej';
         let messDiv = document.createElement('div');
         messDiv.classList.add("message-item");
-        messDiv.innerText = message;
+        messDiv.innerHTML = `<h2>${message}</h2><h5>${author} | ${new Date().getHours()}:${new Date().getMinutes()}</h5>`;
         chatMessages.append(messDiv)
         chatMessages.scrollTop = chatMessages.scrollHeight;
     });
 
     input.addEventListener("keyup", ({key}) => {
-        if (key === "Enter") {
+        if (key === "Enter" && input.value !== '') {
             // Do work
             socket.emit('chatMessage', input.value )
             input.value = ''
